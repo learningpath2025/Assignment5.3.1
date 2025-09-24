1c423bc9c5505ee2b5507806c69ac3497a719792
learningpath2025 <learningpath2025@outlook.com>
2025-09-24 10:51:23 -0500

    adding alternative method

--- Program.cs
+++ Program.cs
@@ -34,6 +34,40 @@ namespace Assignment5._3._1
         {
             int[] flowerbed = { 1, 0, 0, 0, 1 };
             Console.WriteLine(CanPlaceFlowers(flowerbed, 1));
+            //test();
+        }
+
+
+        //Alternative method
+        public static bool test()
+        {
+            int[] flowerbed = { 1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+            int f = (int)Math.Ceiling((decimal)flowerbed.Length/2);
+            Console.WriteLine(f);
+            int one = 0;
+            int zero = 0;
+            bool plantFlower = false;
+
+            foreach (int i in flowerbed)
+            {
+                if (i == 0)
+                {
+                    zero++;
+                }
+                if (i == 1)
+                {
+                    one++;
+                }
+
+                if (zero > one && i == 0 && i + 1 == 0 && i - 1 == 0)
+                {
+                    plantFlower = true;
+                }
+                else
+                {   plantFlower = false; }
+            }
+            Console.WriteLine(plantFlower);
+            return plantFlower;
         }
     }
 }
